1ef5b3cea64122029e4e74ae99e31811f71cba7c
João Pedro Mairinque <jpmairinque@gmail.com>
2021-06-11 10:11:55 -0300

    a

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ function App() {
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
           ingredients = {recipe.recipe.ingredients}
+          
         />
       ))) : "NO RESULTS!" }
       
